08f376caa708e49906d6cac7bbd98838741eb52a
משה מלכה <mmts12@gmail.com>
2022-11-18 22:06:09 +0200

    first-table

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import { calenderServise } from './services/calenderService';
+
+
+
+function App() {
+
+
+  const [daysInMonthArray, setDaysInMonthArray] = useState(calenderServise.daysInMonth());
+  const [daysInWeek, setDaysInWeek] = useState(calenderServise.daysInWeek());
+
+  useEffect(() => {
+    createDays();
+  }, [])
+
+  const createDays = () => {
+    const d = [...Array(Number(daysInMonthArray[new Date().getMonth()])).keys()]
+    d.shift()
+    setDaysInMonthArray(d);
+  }
+
+
+  const getDaysNumberInMonth = () => {
+    // const getDays = (year, month) => new Date(year, month, 0).getDate()
+
+    // for (let i=0 ; i<12 ;i++){
+    //   console.log(daysArray[i].days);
+    //   daysArray[i]['days'] = getDays(2022, i);
+    // }
+
+    // setDaysInMonthArray(daysArray);
+    // const month = new Date().getMonth()
+    // console.log(month)
+
+    // console.log('2022',daysArray);
+  }
+
+  const getFirstDayInMonth = () => {
+
+  }
+
+
+
+
+
+
+  return (
+    <div className="App">
+      {console.log(new Date().getDay())}
+      <h1>Calender</h1>
+      <div className='calender-container'>
+      
+          <div className='header-days'> {daysInWeek.map((day) => <div className='calender-header-day' key={day}>{day}</div>)}</div>
+          <div className='days-in-month'>{daysInMonthArray.map((day, index) => <div className='day' key={index}>{day}</div>)} </div>
+       
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/calenderService.js
@@ -0,0 +1,17 @@
+
+export const calenderServise = {
+    daysInWeek,daysInMonth
+}
+
+function daysInWeek () {
+
+     return ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'] ;
+
+     for(let i=0;i<3;i++){
+        
+     }
+}
+
+function daysInMonth () {
+    return ['31','31','28','31','30','31','30','31','31','30','31','30']
+}
